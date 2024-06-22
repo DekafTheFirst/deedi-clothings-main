7b6c5c0f1378257a14d15e212d329878b4ef15c3
Destiny Kefas <dekeji1@gmail.com>
2024-06-22 23:35:13 +0100

    home page responsive design started

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,50 +8,62 @@ import { Link } from 'react-router-dom';
 import "./Navbar.scss"
 import Cart from '../Cart/Cart';
 import { useSelector } from 'react-redux';
+import { Menu, MenuOpen } from '@mui/icons-material';
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
-  const products = useSelector(state=>state.cart.products)
+  const products = useSelector(state => state.cart.products)
 
   return (
     <div className='navbar'>
       <div className="wrapper">
         <div className="left">
-          
-          <div className="item">
-            <Link className="link" to="/products/women">Women</Link> 
+          <Menu className='toggle'/>
+          <div className="nav-items">
+            <div className="item">
+              <Link className="link" to="/products/women">Women</Link>
+            </div>
+            <div className="item">
+              <Link className="link" to="/products/men">Men</Link>
+            </div>
+            <div className="item">
+              <Link className="link" to="/products/kids">Kids</Link>
+            </div>
+            <div className="item">
+              <Link className="link" to="/about">About</Link>
+            </div>
+            <div className="item">
+              <Link className="link" to="/contact">Contact</Link>
+            </div>
           </div>
-          <div className="item">
-            <Link className="link" to="/products/men">Men</Link> 
-          </div>
-          <div className="item">
-            <Link className="link" to="/products/kids">Kids</Link> 
-          </div>
-          <div className="item">
-            <Link className="link" to="/about">About</Link> 
-          </div>
+
         </div>
         <div className="center">
-          <Link className="link" to='/'><img src='/img/deedi_transparent.png'/></Link>
+          <Link className="link" to='/'><img src='/img/deedi_transparent.png' /></Link>
         </div>
         <div className="right">
 
-          
-          
-          
+
+
+
           {/* <div className="item">
             <Link className="link" to="/products/3">Stores</Link> 
           </div> */}
           <div className="icons">
-            <SearchIcon />
-            <PersonOutlineIcon />
-            <div className="cartIcon" onClick={()=>setOpen(!open)}>
+            <div className="searchbar">
+              <SearchIcon fontSize='small' className='search-icon' />
+            </div>
+            <div className="user">
+              <PersonOutlineIcon />
+              <span>My Account</span>
+              <KeyboardArrowDownIcon className='down-arrow' fontSize='small' />
+            </div>
+            <div className="cartIcon" onClick={() => setOpen(!open)}>
               <ShoppingCartOutlinedIcon />
               <span>{products.length}</span>
             </div>
-          </div>
-          <div className="item">
-            <Link className="link" to="/contact">Contact</Link> 
+
+
           </div>
         </div>
       </div>
