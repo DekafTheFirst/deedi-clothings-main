b414f110474f3937dc22e0e952c17b08cac7d46e
Destiny Kefas <dekeji1@gmail.com>
2024-07-01 23:38:42 +0100

    final commit

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Banner.css';
+import HomeIcon from '@mui/icons-material/Home';
+import OptimizedImage from '../OptimizedImage/OptimizedImage';
+import KeyboardArrowRightOutlined from '@mui/icons-material/KeyboardArrowRightOutlined';
+
+const Banner = ({ pathname, title }) => {
+    const segments = pathname.split('/').filter(segment => segment !== '');
+    // Split pathname and remove empty segments
+    const formatSegment = (segment) => {
+        return segment.replace(/-/g, ' ')
+            .replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())
+            .replace(/'/g, "'s");
+    };
+
+    return (
+        <nav aria-label="breadcrumb">
+            <ol className="breadcrumb justify-content-center">
+                <li className="breadcrumb-item home"><Link to="/"><HomeIcon fontSize='small' /> </Link></li>
+                {segments.map((segment, index) => (
+                    <div className='breadcrumb-item-wrapper' key={index}>
+                        <KeyboardArrowRightOutlined fontSize='small' className="breadcrumb-separator" />
+                        {index >= segments.length - 1 ? (
+                            <li className="breadcrumb-item last" >
+                                <span>{title ? title : formatSegment(segment)}
+                                </span>
+                            </li>
+                        ) : (
+                            <li className="breadcrumb-item last" >
+                                <Link to={`/${segments.slice(0, index + 1).join('/')}`}>
+                                    {formatSegment(segment)}
+                                </Link>
+                            </li>
+                        )}
+                    </div>
+
+                ))}
+            </ol>
+        </nav>
+    );
+};
+
+export default Banner;
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -6,18 +6,25 @@ import useFetch from '../../hooks/useFetch'
 import { CircularProgress } from '@mui/material'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
 import OptimizedImage from '../../components/OptimizedImage/OptimizedImage'
+import TuneIcon from '@mui/icons-material/Tune';
+import { Close } from '@mui/icons-material'
 
 const Products = () => {
   const [maxPrice, setMaxPrice] = useState(1000)
   const [sort, setSort] = useState("asc")
   const [selectedSubCats, setSelectedSubCats] = useState([])
+  const [filterVisibility, setFilterVisibility] = useState(false)
 
 
   const catSlug = useParams().slug
 
   const { data: category, loading: categoryLoading, error: categoryError } = useFetch(`/categories?populate=*&filters[slug][$eq]=${catSlug}`)
   const { data: subCategories, loading: subCategoriesLoading, error: subCategoriesError } = useFetch(`/sub-categories?[filters] [categories] [slug] [$eq]=${catSlug}`)
-  
+
+  const handleFilterVisibilityToggle = () => {
+    setFilterVisibility(!filterVisibility)
+  }
+
   const handleChange = (e) => {
     const value = e.target.value;
     const isChecked = e.target.checked;
@@ -26,60 +33,73 @@ const Products = () => {
 
   }
 
+
+
   // console.log(selectedSubCats)
 
   return (
     <div className="product-list-page">
 
       <div className="container-fluid">
-        <div className="left">
-          <div className="filterItem">
-            <h4 className="filterTitle">Product Categories</h4>
-            {subCategories?.map((item) =>
-            (<div className="inputItem" key={item.id}>
-              <input type="checkbox" id={item.id} value={item.id} onChange={handleChange} />
-              <label htmlFor={item.id}>{item.attributes.title}</label>
-            </div>)
-            )}
-          </div>
-          <div className="filterItem">
-            <h4 className="filterTitle">Filter by price</h4>
-            <div className="inputItem">
-              <span>0</span>
-              <input
-                type="range"
-                min={0}
-                max={1000}
-                onChange={(e) => setMaxPrice(e.target.value)}
-              />
-              <span>{maxPrice}</span>
+
+        <div className={`filters-wrapper ${filterVisibility ? 'show' : ''}`}>
+          <div className={`filters `} >
+            <div className="heading-mobile-only">
+              <h6 className="filterTitle">Filters</h6>
+              <Close className='close' onClick={handleFilterVisibilityToggle} />
             </div>
-          </div>
-          <div className="filterItem">
-            <h4 className="filterTitle">Sort by</h4>
-            <div className="inputItem">
-              <input
-                type="radio"
-                id="asc"
-                value="asc"
-                name="price"
-                onChange={(e) => setSort("asc")}
-              />
-              <label htmlFor="asc">Price (Lowest first)</label>
+            <div className="filterItem">
+              <h6 className="filterTitle">Product Categories</h6>
+              {subCategories?.map((item) =>
+              (<div className="inputItem" key={item.id}>
+                <input type="checkbox" id={item.id} value={item.id} onChange={handleChange} />
+                <label htmlFor={item.id}>{item.attributes.title}</label>
+              </div>)
+              )}
             </div>
-            <div className="inputItem">
-              <input
-                type="radio"
-                id="desc"
-                value="desc"
-                name="price"
-                onChange={(e) => setSort("desc")}
-              />
-              <label htmlFor="desc">Price (Highest first)</label>
+            <div className="filterItem">
+              <h6 className="filterTitle">Filter by price</h6>
+              <div className="inputItem">
+                <span>0</span>
+                <input
+                  type="range"
+                  min={0}
+                  max={1000}
+                  onChange={(e) => setMaxPrice(e.target.value)}
+                />
+                <span>{maxPrice}</span>
+              </div>
+            </div>
+            <div className="filterItem">
+              <h4 className="filterTitle">Sort by</h4>
+              <div className="inputItem">
+                <input
+                  type="radio"
+                  id="asc"
+                  value="asc"
+                  name="price"
+                  onChange={(e) => setSort("asc")}
+                />
+                <label htmlFor="asc">Price (Lowest first)</label>
+              </div>
+              <div className="inputItem">
+                <input
+                  type="radio"
+                  id="desc"
+                  value="desc"
+                  name="price"
+                  onChange={(e) => setSort("desc")}
+                />
+                <label htmlFor="desc">Price (Highest first)</label>
+              </div>
             </div>
           </div>
         </div>
-        <div className="right">
+        <div className="products">
+          <div className="top">
+            <span className='no-of-products'>{5} Products</span>
+            <button className="btn filter-toggle-btn" onClick={handleFilterVisibilityToggle}>Filters <TuneIcon fontSize='small' /> </button>
+          </div>
           {/* {!category
             ?
             (<CircularProgress />)
@@ -97,8 +117,11 @@ const Products = () => {
               />
             )
           } */}
-        
-            <List catSlug={catSlug} maxPrice={maxPrice} sort={sort} subCats={selectedSubCats} />
+
+          <List catSlug={catSlug} maxPrice={maxPrice} sort={sort} subCats={selectedSubCats} />
+          <List catSlug={catSlug} maxPrice={maxPrice} sort={sort} subCats={selectedSubCats} />
+          <List catSlug={catSlug} maxPrice={maxPrice} sort={sort} subCats={selectedSubCats} />
+          <List catSlug={catSlug} maxPrice={maxPrice} sort={sort} subCats={selectedSubCats} />
 
         </div>
       </div>
